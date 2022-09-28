bf0488377161033169a7fc748f026d247d5d2356
Nithish06 <nithss0404@gmail.com>
2022-09-28 10:51:52 +0530

    Initial comit

---
+++ pages/master-index/MasterIndex.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../../styles/MasterIndex.module.css";
+function MasterIndex() {
+  return (
+    <div>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default MasterIndex;
